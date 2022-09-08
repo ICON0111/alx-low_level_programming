9c6b40b78105cf5fe980e8f6406203a30ed7188c
Icon0111 <iconkinging0@gmail.com>
2022-09-08 11:46:38 -0700

    C2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#!/bin/bash
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
- * Return: always 0
- */
+ * Return: always 0 (Success)
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
