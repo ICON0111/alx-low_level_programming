6723f4a4d0c1ca36084d6a9cbd939ca84c08b2b8
Icon0111 <iconkinging0@gmail.com>
2022-09-08 20:41:27 -0700

    Cc

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
-int main(/*void*/)
 
+/**
+ * main - entry point
+ * Description: Print a string using followed by a new line
+ * Return: 0
+*/
+int main(void)
 {
-	puts(/*"Programming is like building a multilingual puzzle*/)
-		
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
-	{
+}
